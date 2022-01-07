bbec47a1567d4fe369486c5120426233f1f59d65
Micha Wensveen <mail@mwensveen.nl>
2022-01-07 21:18:13 +0100

    refactor Normal

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,17 +21,21 @@ class GildedRose {
                     updateSulfuras(item);
                     break;
                 default:
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    updateNormal(item);
+            }
+        }
+    }
 
-                    item.sellIn = item.sellIn - 1;
+    private void updateNormal(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
 
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
