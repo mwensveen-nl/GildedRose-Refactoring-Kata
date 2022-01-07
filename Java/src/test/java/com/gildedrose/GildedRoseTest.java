05371abdbf5287625820ccdb6cd34f8e0fdd3cb5
Micha Wensveen <mail@mwensveen.nl>
2022-01-07 20:37:25 +0100

    Add test for normal item

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,23 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("foo", 5, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(9, app.items[0].quality);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(6, app.items[0].quality);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(5, app.items[0].quality);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(3, app.items[0].quality);
     }
 
 }
